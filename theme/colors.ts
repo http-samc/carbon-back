733d6e0ab4d2e320e0ad4944e17a399a0ffccc5c
http-samc <67826352+http-samc@users.noreply.github.com>
2022-02-04 22:36:39 -0600

    added basic colors structure

---
+++ theme/colors.ts
@@ -0,0 +1,12 @@
+var Colors = {
+    primary: '#20c997',
+    primaryVariant: '#20c997',
+    secondary: '#0dcaf0',
+    textImportant: '#0d6efd',
+    background: '#fff',
+    error: '#cf6679',
+    onPrimary: '#000000',
+    onBackground: '#222',
+}
+
+export default Colors = Colors
